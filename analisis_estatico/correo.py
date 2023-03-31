3fa2c0c16620d1083e469c85bc52926771559821
Carel Sánchez <52781415+Carel3DS@users.noreply.github.com>
2023-03-31 08:57:59 +0200

    ejercicios 3 y 4

---
+++ analisis_estatico/correo.py
@@ -0,0 +1,14 @@
+# Ejercicio 2: fecha
+import re
+
+texto = input().strip()
+patron = r"\b([a-z]+?)\.([a-z]*?)(\.(\d{4}))?\@((alumnos\.)?urjc\.es)\b"
+results = re.findall(patron, texto, flags=re.M)
+
+for match in results:
+    if match[4] == "alumnos.urjc.es":
+        print(f"alumno {match[1]} matriculado en {match[3]}")
+    else:
+        print(f"profesor {match[0]} apellido {match[1]}")
+
+
---
+++ analisis_estatico/e3_fecha.py
@@ -0,0 +1,8 @@
+# Ejercicio 2: fecha
+import re
+
+texto = input().strip()
+patron2 = r"\b(\d{4})\-(\d{2})\-(\d{2})\b"
+results = re.findall(patron2, texto, flags=re.M)
+
+print(re.sub(patron2, r"\3.\2.\1", texto))
